6fce90ec34ed2c96940d0a9105e002022502f2a9
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-20 17:14:25 -0500

    servivce

--- minerva/src/main/java/com/minerva/application/service/SupplierService.java
+++ minerva/src/main/java/com/minerva/application/service/SupplierService.java
@@ -8,7 +8,7 @@ import com.minerva.domain.constants.Role;
 import com.minerva.domain.services.Result;
 import com.minerva.domain.entities.supplier.Supplier;
 import com.minerva.domain.valueObject.id.AllId;
-import com.minerva.domain.valueObject.id.SupplierName;
+import com.minerva.domain.valueObject.SupplierName;
 import com.minerva.domain.valueObject.id.UserName;
 import com.minerva.domain.exceptions.DomainException;
 import com.minerva.domain.repositories.SupplierRepository;
