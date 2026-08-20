d06349cf3c08779e0434b1d77b38b30118fbce5a
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-20 15:26:19 -0500

    id

--- minerva/src/main/java/com/minerva/domain/valueObject/id/Id.java
+++ minerva/src/main/java/com/minerva/domain/valueObject/id/Id.java
@@ -1,6 +1,10 @@
 package com.minerva.domain.valueObject.id;
 
-public interface Id<I> {
-    I value();
-    String asString();
+import com.minerva.domain.exceptions.NullValueException;
+import com.minerva.domain.valueObject.ValueObject;
+
+public abstract class Id<I> extends ValueObject<I> {
+    public Id(I value) throws NullValueException {
+        super(value);
+    }
 }
