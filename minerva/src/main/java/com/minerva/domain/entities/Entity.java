493ac7bf2e0f5e6f8b35f4e824db7bfaf7112fc5
L <160187610+LuisZambranoRamirez@users.noreply.github.com>
2026-08-20 23:22:54 -0500

    Revert "entity"
    This reverts commit f575254bd0a2ed615c35d366852bb8f53a247f51.

--- minerva/src/main/java/com/minerva/domain/entities/Entity.java
+++ minerva/src/main/java/com/minerva/domain/entities/Entity.java
@@ -13,11 +13,6 @@ public abstract class Entity<I extends Id<?>> {
         this.id = id;
     }
 
-    // Ojo, no se si esto da el nombrte de la clase abstracta o el nombre de la que la implemente
-    public String getEntityName() {
-        return getClass().getSimpleName();
-    }
-
     public I getId() {
         return id;
     }
